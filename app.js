1cd12d59e93a3b0da1661efce20495c791f23376
unknown <izacarmo93@gmail.com>
2025-11-26 21:27:02 -0300

    banco de dados

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
